8661869a1a8373a5121f8016c93554b24f63a77f
Mark Delk <jethrodaniel@gmail.com>
2021-09-14 15:12:32 -0500

    basic install

--- install.sh
+++ install.sh
@@ -21,6 +21,14 @@ echo "=== Dockerizing Rails... ==="
 ensure_cmd_exists "docker"
 ensure_cmd_exists "docker-compose"
 
-# git clone https://github.com/jethrodaniel/docker-rails \
-
-echo -e "=== Done ==="
+git clone https://github.com/jethrodaniel/docker-rails .docker-rails \
+  && cp -v .docker-rails/docker-compose.yml . \
+  && cp -v .docker-rails/Dockerfile . \
+  && cp -v .docker-rails/.dockerignore . \
+  && cp -v .docker-rails/docker-compose.yml . \
+  && cp -v .docker-rails/docker-entrypoint.sh . \
+  && cp -v .docker-rails/.env . \
+  && cp -v .docker-rails/.pryrc . \
+  && cp -v .docker-rails/webpack.js config/webpack/development.js \
+  && rm -fr .docker-rails \
+  && echo -e "=== Done ==="
